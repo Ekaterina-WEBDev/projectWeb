7e9c1b240b1c33baa61d0d62e56df511f649f1e0
Ekaterina-WEBDev <5nscux5rauml@mail.ru>
2022-11-26 15:33:56 +0600

    slider + team + price + clients

--- marketing/js/script.js
+++ marketing/js/script.js
@@ -1,3 +1,10 @@
 $(function () {
     var mixer = mixitup('.portfolio__works');
+
+    $('.news__items').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="img/main/arrow-left.svg" alt="arrow left"></button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="img/main/arrow-right.svg" alt="arrow right"></button>'
+    });
+
 })
