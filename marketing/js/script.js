245774d250ac6e81f2ee1351f172261983242cb6
Ekaterina-WEBDev <5nscux5rauml@mail.ru>
2022-12-03 12:57:11 +0600

    адаптивная шапка

--- marketing/js/script.js
+++ marketing/js/script.js
@@ -7,4 +7,21 @@ $(function () {
         nextArrow: '<button type="button" class="slick-next"> <img src="img/main/arrow-right.svg" alt="arrow right"></button>'
     });
 
+
+    /* Бургерное меню */
+    let menu = document.querySelector('.header__burger');
+    let nav = document.querySelector('.header__nav');
+
+    menu.addEventListener('click', function () {
+        nav.classList.toggle('header__nav--active');
+    })
+
+
+    /*
+        Альтернативная запись кнопки бургера:
+        $('.header__burger').on('click', function(){
+            $('.header__items').toggleClass('header__burger--active');
+        })
+    */
+
 })
