87f3c7807d687172cd5d166e97405082325d8963
Ekaterina-WEBDev <5nscux5rauml@mail.ru>
2023-01-17 17:07:53 +0600

    Адаптивная верстка макета to2rent

---
+++ to2rent/js/script.js
@@ -0,0 +1,6 @@
+$('.intro__items').slick({
+    arrows: true,
+    dots: false,
+    prevArrow: '<button class="intro__btn intro__btn--left"></button>',
+    nextArrow: '<button class="intro__btn intro__btn--right"></button>'
+})
